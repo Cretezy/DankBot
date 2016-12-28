3aa800168c34afb95e4e79ba03a11f711487fe92
Charles Crete <cretezy@gmail.com>
2016-12-27 20:27:08 -0500

    Add reddit env info

--- dankbot/__init__.py
+++ dankbot/__init__.py
@@ -1,15 +1,18 @@
 import os
+
 import praw
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from pymessenger import Bot
 from imgurpython import ImgurClient
+from pymessenger import Bot
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 db = SQLAlchemy(app)
 bot = Bot(os.environ.get("MESSENGER_TOKEN"))
-reddit = praw.Reddit(user_agent='dank-420')
+reddit = praw.Reddit(user_agent='dank-420',
+                     client_id=os.environ.get("REDDIT_ID"),
+                     client_secret=os.environ.get("REDDIT_SECRET"))
 imgur = ImgurClient(os.environ.get("IMGUR_ID"), os.environ.get("IMGUR_SECRET"))
 
 from dankbot import models
