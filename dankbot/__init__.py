ee449cb8c7cf3a4bf9eaee931bc96702752e15b9
Charles Crete <cretezy@gmail.com>
2017-01-02 18:37:34 -0500

    Fix import for psycopg2

--- dankbot/__init__.py
+++ dankbot/__init__.py
@@ -6,6 +6,14 @@ from flask_sqlalchemy import SQLAlchemy
 from imgurpython import ImgurClient
 from pymessenger import Bot
 
+try:
+    import psycopg2
+except ImportError:
+    # Fall back to psycopg2cffi
+    from psycopg2cffi import compat
+    compat.register()
+
+
 app = Flask(__name__, static_folder='public')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 db = SQLAlchemy(app)
